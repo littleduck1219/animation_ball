c4d8ec086191c90bb92c57a6ca4ad3ae02b2132a
littleduck <littleduck1219@gmail.com>
2025-06-17 23:09:12 +0900

    웨이브 페이지

---
+++ react/src/pages/wave/WavePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import useCanvasApp from "../../hooks/useCanvasApp";
+
+export default function WavePage() {
+    const { canvas, ctx, stageWidth, stageHeight, startAnimation, stopAnimation } = useCanvasApp();
+    return <div></div>;
+}
